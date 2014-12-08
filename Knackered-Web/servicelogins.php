f1ca1b45effa7c6445ff037cfb51046278e34b8f
andolojm <andolojm@alfredstate.edu>
2014-12-08 09:02:25 -0500

    Team registration improvements

---
+++ Knackered-Web/servicelogins.php
@@ -0,0 +1,8 @@
+<?php
+include('views/header.php');
+include('views/navbar.php');
+?>
+<p>
+    Service logins have not yet been implemented because Nagios-Plugins checks do not use them.
+    In the future, custom checks will be developed and this page will have a purpose.
+</p>
--- Knackered-Web/views/register.php
+++ Knackered-Web/views/register.php
@@ -15,21 +15,30 @@ if (isset($registration)) {
 ?>
 
 <!-- register form -->
-<form method="post" action="register.php" >
+<form method="post" action="register.php" id="registerform">
 
     <!-- the user name input field uses a HTML5 pattern check -->
-    <label for="login_input_username">Username: </label>
-    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+    <label for="login_input_username">Username: </label><br /><br />
+    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required /><br />
+
+<br /><br />
 
     <!-- email field replaced with teamid field -->
-    <label for="login_team_id">Team ID: (Integer, 0 for whiteteam)</label>
+    <label for="login_team_id">Team ID: (Integer, 0 for whiteteam)</label><br />
     <input id="login_team_id" class="login_input" type="text" name="team_id" required />
 
-    <label for="login_input_password_new">Password: </label>
+<br /><br />
+
+    <label for="login_input_password_new">Password: </label><br />
     <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
 
-    <label for="login_input_password_repeat">Repeat password: </label>
+<br /><br />
+
+    <label for="login_input_password_repeat">Repeat password: </label><br />
     <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+
+<br /><br />
+
     <input type="submit"  name="register" value="Register" />
 </form>
 
